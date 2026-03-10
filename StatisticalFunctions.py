bceb1713791e0fa9ffa46a25633925a91aa0f1b6
bahati <bahatifidelis2004@gmail.com>
2026-03-10 11:16:07 +0300

    Add statistical functions for calculating mean, standard deviation, max, and min of a numpy array

---
+++ StatisticalFunctions.py
@@ -0,0 +1,18 @@
+import numpy as np 
+a = np.array([1, 6, 1, 0])
+
+#To get avarage of array
+mean=a.mean()
+print(mean)
+
+#To get standard deviation of array
+std=a.std()
+print(std)
+
+#To get largest and smallest value in array
+max=a.max()
+min=a.min()
+print(max)
+print(min)
+
+
